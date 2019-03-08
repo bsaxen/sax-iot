c9997cd6ebf6908b17c0bee3ae8e5d6b9a78c9e2
bsaxen <benny.saxen@gmail.com>
2019-03-08 17:34:09 +0100

    Update httpClient.c

--- c/httpClient.c
+++ c/httpClient.c
@@ -14,21 +14,24 @@ struct Data d1;
 String stat_url = " ";
 String dyn_url = " ";
 String pay_url = " ";
-String payload = "{}";
+String log_url = " ";
+String payload = "{\"no_data\":\"0\"}";
+String message = "nothing";
 //=============================================
 void setup() {
 //=============================================  
-  c1.conf_id         = "use-mac-address";
+  c1.conf_id         = "5678";
   c1.conf_period     = 10;
   c1.conf_wrap       = 999999;
   c1.conf_feedback   = 1;
 
-  c1.conf_tags       = "tag1,tag2,tag3";
-  c1.conf_desc       = "your_description";
+  c1.conf_tags       = "tag1";
+  c1.conf_desc       = "your";
   c1.conf_platform   = "esp8266";
-  c1.conf_ssid       = "ertyerty";
-  c1.conf_password   = "etyrt";
-  c1.conf_host       = "localhost/git/sax-iot";
+  c1.conf_ssid       = "bridge";
+  c1.conf_password   = "sdfsdf";
+  c1.conf_domain     = "iot.simuino.com";
+  c1.conf_server     = "gateway.php";
   c1.conf_streamId   = "....................";
   c1.conf_privateKey = "....................";
   
@@ -40,6 +43,7 @@ void setup() {
   d1.counter = 0;
   
   stat_url = lib_buildUrlStatic(c1);
+  Serial.println(stat_url);
   String dont_care = lib_wifiConnectandSend(c1,d1, stat_url);
 }
 
@@ -54,11 +58,17 @@ void loop() {
   if (d1.counter > c1.conf_wrap) d1.counter = 1;
   
   dyn_url    = lib_buildUrlDynamic(c1, d1);  
+  Serial.println(dyn_url);
   String msg = lib_wifiConnectandSend(c1,d1, dyn_url);
   Serial.println(msg);
   delay(500);
   pay_url    = lib_buildUrlPayload(c1, d1,payload);  
-  String msg = lib_wifiConnectandSend(c1,d1, pay_url);
+  msg = lib_wifiConnectandSend(c1,d1, pay_url);
+  Serial.println(msg);
+
+
+  log_url = lib_buildUrlLog(c1,message);
+  msg = lib_wifiConnectandSend(c1,d1, log_url);
   Serial.println(msg);
 }
 //=============================================
