e411c88d1f941b628a1545c0da5f82957063f80a
bsaxen <benny.saxen@gmail.com>
2019-03-17 10:21:35 +0100

    Update httpClient.c

--- c/httpClient.c
+++ c/httpClient.c
@@ -1,6 +1,6 @@
 //=============================================
 // File.......: httpClient.c
-// Date.......: 2019-03-11
+// Date.......: 2019-03-17
 // Author.....: Benny Saxen
 // Description: http client
 int sw_version = 1;
@@ -9,8 +9,8 @@ int sw_version = 1;
 //=============================================
 //#include "iotLib.c"
 
-struct Configuration c1;
-struct Data d1;
+struct Configuration co;
+struct Data da;
 //=============================================
 String stat_url = " ";
 String dyn_url = " ";
@@ -21,58 +21,56 @@ String message = "nothing";
 //=============================================
 void setup() {
 //=============================================  
-  c1.conf_sw         = sw_version;
-  c1.conf_id         = "set_to_mac";
-  c1.conf_period     = 10;
-  c1.conf_wrap       = 999999;
-  c1.conf_feedback   = 1;
+  co.conf_sw         = sw_version;
+  co.conf_id         = "set_to_mac";
+  co.conf_period     = 60;
+  co.conf_wrap       = 999999;
+  co.conf_feedback   = 1;
 
-  c1.conf_title      = "title";
-  c1.conf_tags       = "tag1";
-  c1.conf_desc       = "your";
-  c1.conf_platform   = "esp8266";
-  c1.conf_ssid       = "bridge";
-  c1.conf_password   = "sdfsdf";
-  c1.conf_domain     = "iot.simuino.com";
-  c1.conf_server     = "gateway.php";
-  c1.conf_streamId   = "....................";
-  c1.conf_privateKey = "....................";
+  co.conf_title      = "test1";
+  co.conf_tags       = "test1";
+  co.conf_desc       = "test1";
+  co.conf_platform   = "esp8266";
+  co.conf_ssid       = "bridge";
+  co.conf_password   = "dfgdfg";
+  co.conf_domain     = "iot.simuino.com";
+  co.conf_server     = "gateway.php";
+  co.conf_streamId   = "....................";
+  co.conf_privateKey = "....................";
+
+  lib_setup(&co, &da);
   
-  Serial.begin(9600);
-  delay(100);
-  
-  lib_wifiBegin(&c1);
-  
-  d1.counter = 0;
-  c1.conf_id = c1.conf_mac;
-  stat_url = lib_buildUrlStatic(c1);
-  Serial.println(stat_url);
-  String dont_care = lib_wifiConnectandSend(c1,d1, stat_url);
 }
 
 
 //=============================================
 void loop() {
 //=============================================
-  delay(c1.conf_period*1000);
+  String msg;
+  delay(co.conf_period*1000);
   
-  ++d1.counter;
-  d1.rssi = WiFi.RSSI();
-  if (d1.counter > c1.conf_wrap) d1.counter = 1;
+  ++da.counter;
+  da.rssi = WiFi.RSSI();
+  if (da.counter > co.conf_wrap) da.counter = 1;
   
-  dyn_url    = lib_buildUrlDynamic(c1, d1);  
-  Serial.println(dyn_url);
-  String msg = lib_wifiConnectandSend(c1,d1, dyn_url);
+  dyn_url    = lib_buildUrlDynamic(&co, &da);  
+  msg = lib_wifiConnectandSend(&co,&da, dyn_url);
   Serial.println(msg);
-  delay(500);
-  pay_url    = lib_buildUrlPayload(c1, d1,payload);  
-  msg = lib_wifiConnectandSend(c1,d1, pay_url);
-  Serial.println(msg);
+  
+  if (da.counter%10 == 0)
+  { 
+    pay_url    = lib_buildUrlPayload(&co,&da,payload);  
+    msg = lib_wifiConnectandSend(&co,&da, pay_url);
+    Serial.println(msg);
+  }
 
+  if (da.counter%50 == 0)
+  {
+    log_url = lib_buildUrlLog(&co,message);
+    msg = lib_wifiConnectandSend(&co,&da, log_url);
+    Serial.println(msg);
+  }
 
-  log_url = lib_buildUrlLog(c1,message);
-  msg = lib_wifiConnectandSend(c1,d1, log_url);
-  Serial.println(msg);
 }
 //=============================================
 // End of File
