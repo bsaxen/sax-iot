539be994bc08bdc566b081fed292e5fbc540757a
bsaxen <benny.saxen@gmail.com>
2019-03-26 13:30:15 +0100

    Update switch.c

--- c/switch.c
+++ c/switch.c
@@ -3,15 +3,11 @@
 // Date.......: 2019-02-26
 // Author.....: Benny Saxen
 // Description:
+int sw_version = 1;
 //=============================================
 // Configuration
 //=============================================
 //#include "iotLib.c"
-
-struct Configuration c1;
-struct Data d1;
-String stat_url;
-String dyn_url;
 //=============================================
 
 #define FAN_PIN 5  // D1 pin on NodeMCU 1.0
@@ -20,26 +16,25 @@ int g_status = 1;
 //=============================================
 void setup() {
 //=============================================
-  c1.conf_topic = "nytomta/gow/qwe/0";
-  c1.conf_period  = 60;
-  c1.conf_wrap    = 999999;
-  c1.conf_action  = 2;
+  co.conf_sw         = sw_version;
+  co.conf_id         = "set_to_mac";
+  co.conf_period     = 60;
+  co.conf_wrap       = 999999;
+  co.conf_feedback   = 1;
 
-  c1.conf_tags = "tag1,tag2,tag3";
-  c1.conf_desc = "floor_fan";
-  c1.conf_platform = "esp8266";
-  c1.conf_ssid       = "ASUS-hus";
-  c1.conf_password   = "qweqwe";
-  c1.conf_host       = "gow.qwe.com";
-  c1.conf_streamId   = "....................";
-  c1.conf_privateKey = "....................";
+  co.conf_title      = "test1";
+  co.conf_tags       = "test1";
+  co.conf_desc       = "test1";
+  co.conf_platform   = "esp8266";
+  co.conf_ssid       = "bridge";
+  co.conf_password   = "dfgdfg";
+  co.conf_domain     = "iot.simuino.com";
+  co.conf_server     = "gateway.php";
+  co.conf_streamId   = "....................";
+  co.conf_privateKey = "....................";
 
-  d1.fail = 0;
-  Serial.begin(9600);
-  delay(100);
-
-  lib_wifiBegin(c1);
-
+  lib_setup(&co, &da);
+  
   pinMode(FAN_PIN, OUTPUT);
   digitalWrite(FAN_PIN,HIGH);
   delay(2000);
@@ -47,37 +42,62 @@ void setup() {
   delay(2000);
   digitalWrite(FAN_PIN,HIGH);
   g_status = 1;
-
-  stat_url = lib_buildUrlStatic(c1);
-  String dont_care = lib_wifiConnectandSend(c1,d1, stat_url);
 }
 
 //=============================================
 void loop()
 //=============================================
 {
-  ++d1.counter;
-  if (d1.counter > c1.conf_wrap) d1.counter = 1;
-  d1.rssi = WiFi.RSSI();
-  dyn_url = lib_buildUrlDynamic(c1, d1);
-  dyn_url += "&payload=";
-  dyn_url += "{";
-  dyn_url += "\"status\":\"";
-  dyn_url += g_status;
-  dyn_url += "\"}";
-  String msg = lib_wifiConnectandSend(c1,d1, dyn_url);
+  
+  String msg;
+  delay(co.conf_period*1000);
+  
+  ++da.counter;
+  da.rssi = WiFi.RSSI();
+  if (da.counter > co.conf_wrap) da.counter = 1;
+  
+  dyn_url    = lib_buildUrlDynamic(&co, &da);  
+  msg = lib_wifiConnectandSend(&co,&da, dyn_url);
+  Serial.println(msg);
   int res = lib_decode_ON_OFF(msg);
+  int event = 0;
   if (res == 1) 
   {
     digitalWrite(FAN_PIN,HIGH);
     g_status = res;
+    event = 1;
   }
   if (res == 2) 
   {
     digitalWrite(FAN_PIN,LOW);
     g_status = res;
+    event = 2;
   }
-  delay(c1.conf_period*1000);
+  
+  if (event == 1)
+  { 
+  }
+  if (event == 1)
+  { 
+    pay_url    = lib_buildUrlPayload(&co,&da,payload);  
+    msg = lib_wifiConnectandSend(&co,&da, pay_url);
+    Serial.println(msg);
+    
+    String message  = 
+    log_url = lib_buildUrlLog(&co,message);
+    msg = lib_wifiConnectandSend(&co,&da, log_url);
+    Serial.println(msg);
+  }
+
+  payload = "{";
+  payload += "\"status\":\"";
+  payload += g_status;
+  payload += "\"";
+  payload += "}";
+  
+  String msg = lib_wifiConnectandSend(c1,d1, dyn_url);
+
+
 }
 //=============================================
 // End of File
