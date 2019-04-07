b314aeb12be7a7960febedcdb041ca75a4617958
bsaxen <benny.saxen@gmail.com>
2019-04-07 07:10:41 +0200

    Update mapAjax.php

--- mapAjax.php
+++ mapAjax.php
@@ -1,1 +1,19 @@
-dsf
+<?php
+$label = $_GET['label'];
+
+$data = array();
+
+$file = $label.'.map';
+$fh = fopen($file, "r");
+if ($file)
+{
+    while(! feof($fh))
+    {
+      $data = fgets($fh);
+      //$data = explode(",", $line);
+    }
+    fclose($fh);
+}
+print $data;
+return;
+?>
