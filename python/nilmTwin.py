e7018f448fcb20d2914a359e14cad2eed97b1531
bsaxen <benny.saxen@gmail.com>
2019-04-12 19:45:42 +0200

    Update nilmTwin.py

--- python/nilmTwin.py
+++ python/nilmTwin.py
@@ -108,11 +108,11 @@ while True:
     payload += '"pulses1": "'  + str(pulses1)  + '",\n'
     payload += '"counter1": "' + str(counter1) + '",\n'
     payload += '"missed1": "'  + str(missed1)  + '",\n'
-    payload += '"kwh1": "'     + str(kwh1)     + '",\n'
+    payload += '"wh1": "'      + str(wh1)      + '",\n'
     payload += '"pulses2": "'  + str(pulses2)  + '",\n'
     payload += '"counter2": "' + str(counter2) + '",\n'
     payload += '"missed2": "'  + str(missed2)  + '",\n'
-    payload += '"wh2": "'      + str(wh2)     + '"\n'
+    payload += '"wh2": "'      + str(wh2)      + '"\n'
     payload += '}\n'
 
     msg = lib_publishMyPayload(co,dy,payload)
