772dc6ad8c5feb8262b4f645e8b5595a8656b2ed
bsaxen <benny.saxen@gmail.com>
2019-04-12 19:32:33 +0200

    Update nilmTwin.py

--- python/nilmTwin.py
+++ python/nilmTwin.py
@@ -3,9 +3,14 @@
 # Author: Benny Saxen
 # Date: 2019-04-12
 # Description:
+# Nilm Station 1
 # data 0 = nilm period
 # data 1 = nilm counter
 # data 2 = nilm pulses
+# Nilm Station 2
+# data 3 = nilm period
+# data 4 = nilm counter
+# data 5 = nilm pulses
 # =============================================
 from iotLib import *
 #===================================================
@@ -14,18 +19,32 @@ from iotLib import *
 confile = "nilmtwin.conf"
 version = 1
 lib_setup(co,confile,version)
-if co.ndata != 3:
-   print("Configuration missmatch  c_data")
+if co.ndata != 6:
+   print("Configuration missmatch  c_data " + str(co.ndata))
    exit()
 
-missed = 0
-kwh = 0
+missed1  = 0
+wh1     = 0
+counter1 = 0
+pulses1  = 0
+
+missed2  = 0
+wh2     = 0
+counter2 = 0
+pulses2  = 0
 
 error = lib_readData(co,0)
 if error == 0:
-   period = co.myresult
+   period1 = int(co.myresult)
 else:
-   print("Error reading period")
+   print("Error reading period1")
+   exit()
+
+error = lib_readData(co,3)
+if error == 0:
+   period2 = int(co.myresult)
+else:
+   print("Error reading period2")
    exit()
 #===================================================
 # Loop
@@ -36,45 +55,73 @@ while True:
     lib_increaseMyCounter(co,dy)
 
     msg = lib_publishMyDynamic(co,dy)
-
-    prev_counter = counter
+   #================================================== 
+    prev_counter1 = counter1
     error = lib_readData(co,1)
     if error == 0:
-       counter = co.myresult
+       counter1 = int(co.myresult)
     else:
-       print("Error reading counter")
-    
-    diff = counter  - prev_counter
+       print("Error reading counter1")
+
+    diff = int(counter1)  - int(prev_counter1)
     if diff == 1:
        error = lib_readData(co,2)
        if error == 0:
-          pulses = co.myresult
-	  kwh += pulses
-	  roger = 1
+          pulses1 = int(co.myresult)
+          wh1 += int(pulses1)
+          roger = 1
        else:
-	   print("Error reading pulses")
-    else diff > 1:
-       print ("Missed information: " + str(diff))
-       missed += 1
+          print("Error reading pulses1")
+    elif diff > 1:
+       print ("Missed information1: " + str(diff))
+       missed1 += 1
+       message = 'Missed information1' + str(diff)
+       lib_publishMyLog(co, message)
     else:
-       print("Information sync problem: " + str(diff))
+       print("Information sync problem1: " + str(diff))
+   #================================================== 
+    prev_counter2 = counter2
+    error = lib_readData(co,4)
+    if error == 0:
+       counter2 = int(co.myresult)
+    else:
+       print("Error reading counter2")
     
-	
+    diff = int(counter2)  - int(prev_counter2)
+    if diff == 1:
+       error = lib_readData(co,5)
+       if error == 0:
+          pulses2 = int(co.myresult)
+          wh2 += int(pulses2)
+          roger = 1
+       else:
+          print("Error reading pulses2")
+    elif diff > 1:
+       print ("Missed information2: " + str(diff))
+       missed2 += 1
+       message = 'Missed information2' + str(diff)
+       lib_publishMyLog(co, message)
+    else:
+       print("Information sync problem2: " + str(diff))
+   #================================================== 
     payload  = '{\n'
-    payload += '"pulses": "'  + str(pulses)  + '",\n'
-    payload += '"counter": "' + str(counter) + '",\n'
-    payload += '"missed": "'  + str(missed)  + '",\n'
-    payload += '"kwh": "'     + str(123)     + '"\n'
+    payload += '"pulses1": "'  + str(pulses1)  + '",\n'
+    payload += '"counter1": "' + str(counter1) + '",\n'
+    payload += '"missed1": "'  + str(missed1)  + '",\n'
+    payload += '"kwh1": "'     + str(kwh1)     + '",\n'
+    payload += '"pulses2": "'  + str(pulses2)  + '",\n'
+    payload += '"counter2": "' + str(counter2) + '",\n'
+    payload += '"missed2": "'  + str(missed2)  + '",\n'
+    payload += '"wh2": "'      + str(wh2)     + '"\n'
     payload += '}\n'
 
     msg = lib_publishMyPayload(co,dy,payload)
 
     lib_commonAction(co,msg)
 
-    message = 'counter:' + str(dy.mycounter)
-    lib_publishMyLog(co, message)
+    #message = 'counter:' + str(dy.mycounter)
+    #lib_publishMyLog(co, message)
 
-    print ("sleep: " + str(co.myperiod) + " triggered: " + str(dy.mycounter))
     time.sleep(float(co.myperiod))
 #===================================================
 # End of file
