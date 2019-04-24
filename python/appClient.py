4c1b8d7edb2ad215b6aea40088c4f817d442ce3f
bsaxen <benny.saxen@gmail.com>
2019-04-24 17:26:41 +0200

    Update appClient.py

--- python/appClient.py
+++ python/appClient.py
@@ -11,14 +11,27 @@ from appLib import *
 confile = "appclient.conf"
 version = 1
 lib_setup(co,confile,version)
+lib_matrix(co)
+#
+#       app1  app2    app3
+# app1   -    temp1   temp1
+# app2
+# app3 
 #===================================================
 # Loop
 #===================================================
 while True:
     
     lib_increaseMyCounter(co,dy)
-    payload = '{"test":"10043","test2": "453"}'
+    
+    payload = '{"channel":"1","test": "123"}'
     msg = lib_publishMychannel(co,dy,1,payload)
+    
+    payload = '{"channel":"2","test": "321"}'
+    msg = lib_publishMychannel(co,dy,2,payload)
+    
+    
+    
     message = 'counter:' + str(dy.mycounter)
     lib_publishMyLog(co, message)
     time.sleep(float(co.myperiod))
