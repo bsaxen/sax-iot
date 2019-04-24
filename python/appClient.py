64faf57b3bff9af13473ffe19e50c4afd7298df7
bsaxen <benny.saxen@gmail.com>
2019-04-24 21:09:39 +0200

    Update appClient.py

--- python/appClient.py
+++ python/appClient.py
@@ -16,26 +16,31 @@ lib_matrix(co)
 #       app1  app2    app3
 # app1   -    temp1   temp1
 # app2
-# app3 
+# app3
 #===================================================
 # Loop
 #===================================================
 while True:
-    
+
     lib_increaseMyCounter(co,dy)
-    
+
     payload = '{"channel":"1","test": "123"}'
     msg = lib_publishMyChannel(co,dy,1,payload)
-    
+
+
     payload = '{"channel":"2","test": "321"}'
     msg = lib_publishMyChannel(co,dy,2,payload)
+
+
+    payload = '{"title":"3","test": "benny"}'
+    msg = lib_publishMyChannel(co,dy,3,payload)
+
     
-    
-    
+
     message = 'counter:' + str(dy.mycounter)
     lib_publishMyLog(co, message)
     time.sleep(float(co.myperiod))
-    
+
 #===================================================
 # End of file
 #===================================================
