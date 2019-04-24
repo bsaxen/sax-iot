e78bc532f179e70301d3e12b18cfe7d2f34a653c
bsaxen <benny.saxen@gmail.com>
2019-04-24 17:14:57 +0200

    Update appClient.py

--- python/appClient.py
+++ python/appClient.py
@@ -4,32 +4,25 @@
 # Date: 2019-04-24
 # Description:
 # =============================================
-from iotLib import *
+from appLib import *
 #===================================================
 # Setup
 #===================================================
-confile = "httpclient.conf"
+confile = "appclient.conf"
 version = 1
 lib_setup(co,confile,version)
 #===================================================
 # Loop
 #===================================================
 while True:
+    
     lib_increaseMyCounter(co,dy)
-
-    msg = lib_publishMyDynamic(co,dy)
-
     payload = '{"test":"10043","test2": "453"}'
-    print payload
-    msg = lib_publishMyPayload(co,dy,payload)
-
-    lib_commonAction(co,msg)
-
+    msg = lib_publishMychannel(co,dy,1,payload)
     message = 'counter:' + str(dy.mycounter)
     lib_publishMyLog(co, message)
-
-    print "sleep: " + str(co.myperiod) + " triggered: " + str(dy.mycounter)
     time.sleep(float(co.myperiod))
+    
 #===================================================
 # End of file
 #===================================================
