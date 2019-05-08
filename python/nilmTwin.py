75357de43ed03a512441870beea143f98111ca28
bsaxen <benny.saxen@gmail.com>
2019-05-08 17:55:07 +0200

    Update nilmTwin.py

--- python/nilmTwin.py
+++ python/nilmTwin.py
@@ -1,7 +1,7 @@
 # =============================================
 # File: nilmTwin.py
 # Author: Benny Saxen
-# Date: 2019-05-07
+# Date: 2019-05-08
 # Description:
 # Nilm Station 1
 # data 0 = nilm period
@@ -78,12 +78,12 @@ while True:
        if error == 0:
           pulses1 = int(co.myresult)
           wh1 += int(pulses1)
-          lib_writeMemory('wh1.txt',wh1);
+          lib_writeMemory('wh1.txt',str(wh1));
           roger = 1
        else:
           print("Error reading pulses1")
     elif diff > 1:
-       wh1 = lib_readMemory('wh1.txt');
+       wh1 = int(lib_readMemory('wh1.txt'));
        print ("Missed information1: " + str(diff))
        missed1 += 1
        message = 'Missed information1' + str(diff)
@@ -104,12 +104,12 @@ while True:
        if error == 0:
           pulses2 = int(co.myresult)
           wh2 += int(pulses2)
-          lib_writeMemory('wh2.txt',wh2);
+          lib_writeMemory('wh2.txt',str(wh2));
           roger = 1
        else:
           print("Error reading pulses2")
     elif diff > 1:
-       wh2 = lib_readMemory('wh2.txt');
+       wh2 = int(lib_readMemory('wh2.txt'));
        print ("Missed information2: " + str(diff))
        missed2 += 1
        message = 'Missed information2' + str(diff)
