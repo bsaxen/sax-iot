188c8a7bba22ce49487bb74f578e9bd6f31ce7ce
bsaxen <benny.saxen@gmail.com>
2019-05-18 23:32:07 +0200

    Update iotLib.py

--- python/iotLib.py
+++ python/iotLib.py
@@ -707,6 +707,7 @@ def lib_setup(co,confile,version):
 def lib_loop(co,dy):
 #===================================================
     dy.myipaddress = ([l for l in ([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")][:1], [[(s.connect(('8.8.8.8', 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) if l][0][0])
+    print dy.myipaddress
     dy.mycounter += 1
     if dy.mycounter > co.mywrap:
         dy.mycounter = 1
