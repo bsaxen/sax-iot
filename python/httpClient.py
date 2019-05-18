e13dd458a25d00750ad97f60a3f99fa47fb5f5da
bsaxen <benny.saxen@gmail.com>
2019-05-18 23:41:41 +0200

    Update httpClient.py

--- python/httpClient.py
+++ python/httpClient.py
@@ -15,22 +15,11 @@ lib_setup(co,confile,version)
 # Loop
 #===================================================
 while True:
-
     lib_loop(co,dy)   
-    #lib_increaseMyCounter(co,dy)
-
-    #msg = lib_publishMyDynamic(co,dy)
-
     payload = '{"test":"10043","test2": "453"}'
     print payload
     msg = lib_publishMyPayload(co,payload)
-
     lib_commonAction(co,msg)
-
-    message = 'counter:' + str(dy.mycounter)
-    lib_publishMyLog(co, message)
-
-    print "sleep: " + str(co.myperiod) + " triggered: " + str(dy.mycounter)
     time.sleep(float(co.myperiod))
 #===================================================
 # End of file
