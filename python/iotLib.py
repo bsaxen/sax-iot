89ae7e132ecc7935b562ab96c920bc0282d4e94c
bsaxen <benny.saxen@gmail.com>
2019-05-18 23:23:03 +0200

    Update iotLib.py

--- python/iotLib.py
+++ python/iotLib.py
@@ -1,7 +1,7 @@
 # =============================================
 # File: iotLib.py
 # Author: Benny Saxen
-# Date: 2019-05-15
+# Date: 2019-05-18
 # Description: IoT python library
 version = 2
 # =============================================
@@ -37,6 +37,7 @@ class ModuleDynamic:
     mystate   = 0
     mymode    = 0
     mycounter = 0
+    myipaddress = 'ip'
     mystop    = 0
     myerrors  = 0
     mydev_ts = ""
@@ -179,7 +180,7 @@ def lib_publishMyDynamic(co,dy):
     data['id']       = co.myid
     payload  = '{'
     payload += '"counter" : "'   + str(dy.mycounter)       + '",'
-    #payload += '"uplink" : "'    + str(dy.myuplink_time)   + '",'
+    payload += '"ipaddress" : "'    + str(dy.ipaddress)   + '",'
     #payload += '"downlink" : "'  + str(dy.mydownlink_time) + '",'
     payload += '"errors" : "'    + str(dy.myerrors)        + '"'
     payload += '}'
@@ -705,6 +706,7 @@ def lib_setup(co,confile,version):
 #===================================================
 def lib_loop(co,dy):
 #===================================================
+    dy.ipaddress = ([l for l in ([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")][:1], [[(s.connect(('8.8.8.8', 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) if l][0][0])
     dy.mycounter += 1
     if dy.mycounter > co.mywrap:
         dy.mycounter = 1
