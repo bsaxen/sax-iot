fd78a6a3e814703ff5f17fb42afd003ed603659f
bsaxen <benny.saxen@gmail.com>
2019-12-18 21:11:24 +0100

    Update stepperMotor.c

--- c/stepperMotor.c
+++ c/stepperMotor.c
@@ -1,9 +1,11 @@
 //=============================================
 // File.......: stepperMotor.c
-// Date.......: 2019-05-10
+// Date.......: 2019-12-17
 int sw_version = 1;
 // Author.....: Benny Saxen
+//=============================================
 // Description:
+// Note: CW, CCW orientation is from stepper motor facing axis side
 // Message Api:
 // move,dir,step-size,steps,delay  example: 1,1,40,5
 // reset                      reset position to zero
@@ -14,17 +16,17 @@ int sw_version = 1;
 // Debug Codes
 //
 // Normal 
-// 801 Max limit reached
-// 802 Max limit released
-// 803 Min limit reached
-// 804 Min limit released
+// 801 CW limit reached
+// 802 CW limit released
+// 803 CCW limit reached
+// 804 CCW limit released
 // 805 Unknow limit detected
 // 901 CW ok
 // 902 CCW ok
 // Error
-// 9003 Failure during calibration, max limit not found
-// 9004 Failure during calibration, min limit not found
-// 9005 Failure during calibration, min and max limit not found
+// 9003 Failure during calibration, cw limit not found
+// 9004 Failure during calibration, ccw limit not found
+// 9005 Failure during calibration, cw and ccw limit not found
 //=============================================
 //#include "iotLib.c"
 //================================================
@@ -51,37 +53,55 @@ int number_of_steps = 0;
 int delay_between_steps = 10;
 int limit = 901;
 int g_calibrated = 0;
+int g_steps = 0;
+int g_simulated_position = 77;
+int g_cw_ccw = 0; // Normal shunt rotation
 //================================================
 int stepCW(int steps,int dd, int force)
 //================================================
 {
   int i;
+  int buf = 10;
   digitalWrite(DIR, LOW);
   digitalWrite(SLEEP, HIGH); // Set the Sleep mode to AWAKE.
   for(i=0;i<=steps;i++)
     {
+      g_simulated_position++;
       delayMicroseconds(200);
       digitalWrite(STEP, HIGH);
       delay(dd);
       digitalWrite(STEP, LOW);
       delay(dd);
+      //if (g_simulated_position == 222 && force == 0) 
       if (digitalRead(LIMIT) == HIGH && force == 0) 
       {
         digitalWrite(MINMAX, HIGH);
         digitalWrite(DIR, LOW);
         digitalWrite(SLEEP, LOW);
+        g_steps = i;
+        Serial.println("stepCW - limit reached");
         return 801;
       }
-      if (digitalRead(LIMIT) == LOW && force == 1)
+      //if (g_simulated_position > 10 && force == 1 && buf > 0)
+      if (digitalRead(LIMIT) == LOW && force == 1 && buf > 0)
       {
-        digitalWrite(MINMAX, LOW);
-        digitalWrite(DIR, LOW);
-        digitalWrite(SLEEP, LOW);
-        return 804;
+        buf--;
+        Serial.println(buf);
+        if( buf == 0)
+        {
+          digitalWrite(MINMAX, LOW);
+          digitalWrite(DIR, LOW);
+          digitalWrite(SLEEP, LOW);
+          g_steps = i;
+          Serial.println("stepCW - limit released");
+          return 804;
+        }
       }
     }
   digitalWrite(DIR, LOW);
-  digitalWrite(SLEEP, LOW); // Set the Sleep mode to SLEEP.Serial.println
+  digitalWrite(SLEEP, LOW); // Set the Sleep mode to SLEEP
+  g_steps = i;
+  Serial.println(steps);
   return steps;
 }
 
@@ -91,32 +111,47 @@ int stepCCW(int steps,int dd, int force)
 
 {
   int i;
+  int buf = 10;
   digitalWrite(DIR, HIGH);
   digitalWrite(SLEEP, HIGH); // Set the Sleep mode to AWAKE.
   for(i=0;i<steps;i++)
     {
+      g_simulated_position--;
       delayMicroseconds(200);
       digitalWrite(STEP, HIGH);
       delay(dd);
       digitalWrite(STEP, LOW);
       delay(dd);
+      //if (g_simulated_position == 0 && force == 0)
       if (digitalRead(LIMIT) == HIGH && force == 0)
       {
         digitalWrite(MINMAX, HIGH);
         digitalWrite(DIR, LOW);
         digitalWrite(SLEEP, LOW);
+        g_steps = i;
+        Serial.println("stepCCW - limit reached");
         return 803;
       }
-      if (digitalRead(LIMIT) == LOW && force == 1)
+      //if (g_simulated_position < 211 && force == 1 && buf > 0)
+      if (digitalRead(LIMIT) == LOW && force == 1 && buf > 0)
       {
-        digitalWrite(MINMAX, LOW);
-        digitalWrite(DIR, LOW);
-        digitalWrite(SLEEP, LOW);
-        return 802;
+        buf--;
+        Serial.println(buf);
+        if( buf == 0)
+        {
+          digitalWrite(MINMAX, LOW);
+          digitalWrite(DIR, LOW);
+          digitalWrite(SLEEP, LOW);
+          g_steps = i;
+          Serial.println("stepCCW - limit released");
+          return 802;
+        }
       }
     }
   digitalWrite(DIR, LOW);
   digitalWrite(SLEEP, LOW); // Set the Sleep mode to SLEEP.
+  g_steps = i;
+  Serial.println(steps);
   return steps;
 }
 
@@ -164,11 +199,11 @@ int move_stepper(int dir, int step_size, int number_of_step, int delay_between_s
             {
               sw = 901;
             }
-          // Max position reached, move back from max
+          // Max position reached, move back from CW limit
             if (sw == 801)
             {
                sw = stepCCW(99, delay_between_steps,1);
-               Serial.println("MAX_LIMIT");             
+               Serial.println("CW release");             
             }
         }
         else if(dir == COUNTER_CLOCKWISE)
@@ -180,11 +215,11 @@ int move_stepper(int dir, int step_size, int number_of_step, int delay_between_s
             {
               sw = 902;
             }
-          // Min position reached, move back from min
+          // Min position reached, move back from CCW limit
             if (sw == 803)
             {
                sw = stepCW(99, delay_between_steps,1);
-               Serial.println("MIN_LIMIT");             
+               Serial.println("CCW release");             
             }
         }
         else
@@ -199,64 +234,131 @@ int move_stepper(int dir, int step_size, int number_of_step, int delay_between_s
         return sw;
 }
 //================================================
-int calibrate()
-// Returns number of steps in working interval
+int cw_limit()
+// Returns 802 of ok, else 9003
 //================================================
 {
         int touch1 = 0;
         int touch2 = 0;
         int ok = 0;
-        int both = 0;
-  
-        Serial.println( "FULL STEP");
-        digitalWrite(MS1,LOW);
-        digitalWrite(MS2,LOW);
-
-        Serial.println( "Calibrate max level...");
+        
+        Serial.println( "Calibrate CW level...");
         touch1 = stepCW(400, delay_between_steps,0);
         if (touch1 == 801) 
         {
-          Serial.println("MAX_LIMIT reached");
-          
+          //Serial.println("CW_LIMIT reached");
           touch2 = stepCCW(99, delay_between_steps,1);
+          //Serial.println("CW_LIMIT release value");
+          //Serial.println(touch2);
           if (touch2 == 802)
           {
-             Serial.println( "Released max level");
-          }
-        }
-        else
-        {
-          Serial.println("ERROR: Calibrate max failed");
-          ok = 9003;
-          both = 1;
-        }
-
-        delay(100);
-        
-        Serial.println( "Calibrate zero level...");
-        touch1 = stepCCW(400, delay_between_steps,0);
-        if (touch1 == 803)
-        {
-          Serial.println("MIN_LIMIT reached");
-          touch2 = stepCW(99, delay_between_steps,1);
-          if (touch2 == 804)
-          {
-             Serial.println( "Released min level");
+             //Serial.println( "Released CW limit");
+             Serial.print( "Simulated position = ");
+             Serial.println(g_simulated_position);
              ok = touch2;
           }
         }
         else
         {
-          Serial.println("ERROR: Calibrate min failed");
+          Serial.println("ERROR: Calibrate CW failed");
+          ok = 9003;
+        }
+        return(ok);
+}
+//================================================
+int ccw_limit()
+// Returns 804 of ok, else 9004
+//================================================
+{
+        int touch1 = 0;
+        int touch2 = 0;
+        int ok = 0;
+        
+        Serial.println( "Calibrate CCW level...");
+        touch1 = stepCCW(400, delay_between_steps,0);
+        if (touch1 == 803)
+        {
+          //Serial.println("CCW_LIMIT reached");
+          touch2 = stepCW(99, delay_between_steps,1);
+          if (touch2 == 804)
+          {
+             //Serial.println( "Released CCW level");
+             Serial.print( "Simulated position = ");
+             Serial.println(g_simulated_position);
+             ok = touch2;
+          }
+        }
+        else
+        {
+          Serial.println("ERROR: Calibrate CCW failed");
           ok = 9004;
-          both += 1;
         }
 
-        if (both == 2) ok = 9005;
-        Serial.print( "Calibrate result");
-        Serial.println(ok);
-        
+        return(ok);
+}
+//================================================
+int calibrate(int cw_ccw)
+// Returns number of steps in working interval
+// cw_ccw = 0: CCW hot CW cool (normal)
+// cw_ccw = 1: CCW cool CW hot
+//================================================
+{
+        int both = 0;
+        int range = 0;
+        int ok = 0;
+
+        int res = 0;
+
+        // Full Step
+        Serial.println( "FULL STEP");
+        digitalWrite(MS1,LOW);
+        digitalWrite(MS2,LOW);
   
+        res = 0;
+        if (cw_ccw == 0)
+        {
+          res = ccw_limit();
+        }
+        else
+        {
+          res = cw_limit();
+        }
+        if (res > 9000) both++;
+        
+        delay(100);
+        
+        res = 0;
+        if (cw_ccw == 0)
+        {
+          res = cw_limit();
+        }
+        else
+        {
+          res = ccw_limit();
+        }
+        if (res > 9000) both++;
+
+        
+        if (both == 2) ok = 9005;
+        
+        Serial.print( "Calibration result ");
+        Serial.println(ok);
+
+        if (ok == 0)
+        {
+          if (cw_ccw == 0)
+          {
+            res = stepCCW(30, delay_between_steps,0);
+          }
+          else
+          {
+            res = stepCW(30, delay_between_steps,0);
+          } 
+
+          Serial.print( "Last Stepper move ");
+          Serial.println(res);
+          Serial.println( "At rest ...");
+        }
         return ok;    
 }
 //================================================
@@ -264,7 +366,7 @@ void setup(void){
 //================================================
   co.conf_sw         = sw_version;
   co.conf_id         = "set_to_mac";
-  co.conf_period     = 20;
+  co.conf_period     = 60;
   co.conf_wrap       = 999999;
   co.conf_feedback   = 1;
 
@@ -276,14 +378,14 @@ void setup(void){
   co.conf_domain     = "iot.simuino.com";
   co.conf_server     = "gateway.php";
   
-  co.conf_ssid_1     = "bridge";
-  co.conf_password_1 = "qwer";
+  strcpy(co.conf_ssid_1,"bridge");
+  strcpy(co.conf_password_1,"qwer");
 
-  co.conf_ssid_2     = "NABTON";
-  co.conf_password_2 = "a1b2c3d4e5f6g7";
+  strcpy(co.conf_ssid_2,"NABTON");
+  strcpy(co.conf_password_2,"a1b2c3d4e5f6g7");
   
-  co.conf_ssid_3     = "ASUS";
-  co.conf_password_3 = "123";
+  strcpy(co.conf_ssid_3,"ASUS");
+  strcpy(co.conf_password_3,"123");
 
   lib_setup(&co, &da);
   
@@ -304,9 +406,9 @@ void setup(void){
     digitalWrite(MINMAX,LOW);
   
     //Possible settings are (MS1/MS2) : full step (0,0), half step (1,0), 1/4 step (0,1), and 1/8 step (1,1)
-
+    g_steps = 0;
     delay(10);
-    int ok = calibrate();
+    int ok = calibrate(g_cw_ccw);
     if (ok > 9000)
     {
       Serial.println("Calibration failed");
@@ -392,7 +494,7 @@ void loop(void){
   }
   else if (res == 6614)
   {
-    int ok = calibrate();
+    int ok = calibrate(g_cw_ccw);
     if (ok > 9000)
     {
       Serial.println("Calibration failed");
